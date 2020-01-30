bad164ad2be74bce0e69d5397558e30afde31b34
Caly Yang <caly@canva.com>
2020-01-30 13:23:19 +1100

    Handle special mentions

--- base/formatting.js
+++ base/formatting.js
@@ -7,8 +7,11 @@ const formatText = (text) => {
 const formatTextForMentions = (text) => {
   // To format user mentions correctly, they need to be of form "<@U024BE7LH>": https://api.slack.com/reference/surfaces/formatting#mentioning-users
   const formatForUserMentions = (text) => text.replace(/<@ /g, '<@');
+  // Links to public channels should look like "<#C024BE7LR>" or "<#C024BE7LR|some-channel>"
   const formatForChannelMentions = (text) => text.replace(/<# [A-Z0-9].* \| [a-zA-z0-9-].*>/g, stripWhitespace);
-  return formatForChannelMentions(formatForUserMentions(text));
+  // @here and @channel should look like <!here> and <!channel>, respectively
+  const formatForSpecialMentions = (text) => text.replace(/<! (here|channel)>/, stripWhitespace);
+  return formatForSpecialMentions(formatForChannelMentions(formatForUserMentions(text)));
 }
 
 const formatTextForEmojis = (text) => {

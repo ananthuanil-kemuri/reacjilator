e13c630bedb0eba0d660ca8b148a059a8f91eeda
Caly Yang <caly@canva.com>
2020-01-30 13:47:17 +1100

    Handle inconsistent whitespaces in channel mentions

--- base/formatting.js
+++ base/formatting.js
@@ -8,7 +8,7 @@ const formatTextForMentions = (text) => {
   // To format user mentions correctly, they need to be of form "<@U024BE7LH>": https://api.slack.com/reference/surfaces/formatting#mentioning-users
   const formatForUserMentions = (text) => text.replace(/<@ /g, '<@');
   // Links to public channels should look like "<#C024BE7LR>" or "<#C024BE7LR|some-channel>"
-  const formatForChannelMentions = (text) => text.replace(/<# [A-Z0-9].* \| [a-zA-z0-9-].*>/g, stripWhitespace);
+  const formatForChannelMentions = (text) => text.replace(/<\s*#\s*[A-Z0-9]*\s*\|\s*[a-zA-Z0-9-]*\s*>/g, stripWhitespace);
   // @here and @channel should look like <!here> and <!channel>, respectively
   const formatForSpecialMentions = (text) => text.replace(/<! (here|channel)>/, stripWhitespace);
   return formatForSpecialMentions(formatForChannelMentions(formatForUserMentions(text)));

9b9e00db39f28699991ebf4c97e802ff85dfe4ec
Caly Yang <caly@canva.com>
2020-01-31 14:43:15 +1100

    Add user mentions test; clarify var naming

--- base/tests/formatting.test.js
+++ base/tests/formatting.test.js
@@ -1,24 +1,34 @@
 const { formatText } = require('./../formatting');
 
+test('formatText formats user mentions properly', () => {
+  const inputUserMentionsExpectedOutput = {
+    'Go home <@ USA5H7BUN> <@ US7Q3P2J2>': 'Go home <@USA5H7BUN> <@US7Q3P2J2>',
+    'Go home < @USA5H7BUN> <@US7Q3P2J2>': 'Go home <@USA5H7BUN> <@US7Q3P2J2>',
+  }
+  for (const [input, expectedOutput] of Object.entries(inputUserMentionsExpectedOutput)) {
+    expect(formatText(input)).toBe(expectedOutput);
+  }
+})
+
 test('formatText formats emojis properly', () => {
-  const testEmojisAndResponses = {
+  const inputEmojisExpectedOutput = {
     ': smile:': ':smile:',
     ': globe_with_meridians:': ':globe_with_meridians:',
     ': male-firefighter:': ':male-firefighter:',
     ': male-police-officer :: skin-tone-4:': ':male-police-officer::skin-tone-4:'
   }
-  for (const [input, expectedOutput] of Object.entries(testEmojisAndResponses)) {
+  for (const [input, expectedOutput] of Object.entries(inputEmojisExpectedOutput)) {
     expect(formatText(input)).toBe(expectedOutput);
   }
 })
 
 test('formatText formats links properly', () => {
-  const testLinksAndResponses = {
+  const inputLinksExpectedOutput = {
     '<http: //canva.com': '<http://canva.com',
     '<https: //canva.com': '<https://canva.com',
     '< https: //www.canva.com': '<https://www.canva.com',
   }
-  for (const [input, expectedOutput] of Object.entries(testLinksAndResponses)) {
+  for (const [input, expectedOutput] of Object.entries(inputLinksExpectedOutput)) {
     expect(formatText(input)).toBe(expectedOutput);
   }
 })

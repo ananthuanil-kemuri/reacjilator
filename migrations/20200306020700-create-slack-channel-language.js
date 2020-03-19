3c6fe342b36bf946e1cc67d98b6753e597f9bfc4
Caly Yang <caly@canva.com>
2020-03-19 15:05:15 +1100

    Fix table name in migration

--- migrations/20200306020700-create-slack-channel-language.js
+++ migrations/20200306020700-create-slack-channel-language.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('channel-language', {
+    return queryInterface.createTable('ChannelLanguage', {
       channel_id: {
         allowNull: false,
         primaryKey: true,
@@ -23,6 +23,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('channel-language');
+    return queryInterface.dropTable('ChannelLanguage');
   }
 };

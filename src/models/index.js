cb774db1ac5749a7a8658a83bd550684d79288c7
Caly Yang <caly@canva.com>
2020-03-20 12:03:31 +1100

    Import core-js, regenerator-runtime; update dbConfig import

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,13 @@
 'use strict';
 
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../dbConfig.json')[env];
+const config = require(__dirname + '/../dbConfig.js')[env];
 const db = {};
 
 let sequelize;

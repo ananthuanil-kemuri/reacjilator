b143d98a6374334cf47037ce679eb17a0f5fcf29
Caly Yang <caly@canva.com>
2020-03-20 14:28:40 +1100

    Remove operatorsAliases from dbConfig.js

--- src/dbConfig.js
+++ src/dbConfig.js
@@ -6,14 +6,12 @@ module.exports = {
     "host": "127.0.0.1",
     "port": 6000,
     "dialect": "postgres",
-    "operatorsAliases": false,
     "define": {
       "freezeTableName": true
     }
   },
   "production": {
     "use_env_variable": "DATABASE_URL",
-    "operatorsAliases": false,
     "define": {
       "freezeTableName": true
     }
